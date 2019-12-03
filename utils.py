f3fc2117e492300bbfd4cdd671d08da0821e7cbd
Alessandro Stolfo <alessandro.stolfo@gmail.com>
2019-12-03 13:48:40 +0100

    Age prediction from both EEG and MRI

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ from functools import reduce
 import seaborn as sns
 from sklearn.model_selection import KFold
 from tensorflow.python.keras.activations import tanh
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_absolute_error
 
 def visualize(disease = None, category = None):
     '''
@@ -243,10 +243,47 @@ def create_dataset_eeg_old(disease = None, category = None, SCORE = 'Age', clust
         dataset = dataset.drop([category], axis = 1)
         return dataset
     
+def cv_for_cde(data, labels, name, std, n_splits = 5):
+    '''
+    model: must be a sklearn object with .fit and .predict methods
+    data: the X matrix containing the features, can be a pd.DataFrame or a np object (array or matrix)
+    labels: y, can be a pd.DataFrame or a np array
+    n_splits: number of desired folds
+    => returns array of mean suqared error calculated on each fold
+    '''
+    from cde.density_estimator import MixtureDensityNetwork
     
+    input_dim = data.shape[1]
+    kf = KFold(n_splits=n_splits, shuffle=True)
+    data = np.array(data)
+    labels = np.array(labels)
+    mses = []
+    i = 1
+    for train, test in kf.split(data):
+        model = MixtureDensityNetwork(name=name + str(i),
+                              ndim_x=input_dim,
+                              ndim_y=1,
+                              n_centers=10,
+                              hidden_sizes=(16, 16),
+                              hidden_nonlinearity=tanh,
+                              n_training_epochs=1000,
+                              x_noise_std=std,
+                              y_noise_std=std
+                             )
+        
+        print("Split: {}".format(i), end="\r")
+        X_train, X_test, y_train, y_test = data[train], data[test], labels[train], labels[test]
+        model.fit(X=X_train, Y=y_train, verbose=True)
+        pred = model.mean_(X_test)
+        pred = pred.reshape((-1,1)).flatten()
+        mse = sum((pred - y_test)**2)/len(test)
+        print('MSE: {}'.format(mse))
+        mses.append(mse)
+        i = i+1
+    return mses  
     
 # Helper function for cross-validation
-def cv(model, data, labels, n_splits = 5, want_r2=False):
+def cv(model, data, labels, n_splits = 5, want_r2=False, want_mae=False):
     '''
     model: must be a sklearn object with .fit and .predict methods
     data: the X matrix containing the features, can be a pd.DataFrame or a np object (array or matrix)
@@ -259,6 +296,7 @@ def cv(model, data, labels, n_splits = 5, want_r2=False):
     labels = np.array(labels)
     mses = []
     r2s = []
+    maes = []
     i = 1
     for train, test in kf.split(data):
         print("Split: {}".format(i), end="\r")
@@ -269,10 +307,15 @@ def cv(model, data, labels, n_splits = 5, want_r2=False):
         r2 = r2_score(y_pred=pred, y_true=y_test)
         mses.append(mse)
         r2s.append(r2)
+        maes.append(mean_absolute_error(y_pred=pred, y_true=y_test))
         i = i+1
         
-    if want_r2:
+    if want_r2 and want_mae:
+        return (mses, r2s, maes)
+    elif want_r2:
         return (mses, r2s)
+    elif want_mae:
+        return (mses, maes)
     else:
         return mses
     
